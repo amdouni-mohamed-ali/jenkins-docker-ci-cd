e0f03b100283d670a91492bcb0749a9a3126514f
Mohamed <mamdouni@salto-consulting.com>
2020-03-28 19:02:09 +0100

    [fix] MAI - fix the project name

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -7,4 +7,4 @@ echo "***************************"
 PROJECT_DIR=/home/salto/tutorials/jenkins/jenkins-pipeline-github
 WORKSPACE=$PROJECT_DIR/jenkins_home/workspace/pipeline-jenkins-docker-ci-cd
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v $PROJECT_DIR/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/web-app:/app -v $PROJECT_DIR/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
